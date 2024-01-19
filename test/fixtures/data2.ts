3f25aa90f2621e00bf4a33282f5453205cd532a3
Juan Posso <juan.posso@epn.edu.ec>
2024-01-19 07:53:13 -0500

    test: Added unit tests for functions

---
+++ test/fixtures/data2.ts
@@ -0,0 +1,17 @@
+export const data2 = [
+  {
+    name: "repo1",
+    stargazers_count: 10,
+    updated_at: "2020-11-18T12:00:00Z",
+  },
+  {
+    name: "repo2",
+    stargazers_count: 5,
+    updated_at: "2021-11-18T12:00:00Z",
+  },
+  {
+    name: "repo3",
+    stargazers_count: 1,
+    updated_at: "2022-11-18T12:00:00Z",
+  },
+];
