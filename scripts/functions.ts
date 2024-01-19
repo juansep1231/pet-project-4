86dc567bdb8e7738355aca3e451cf095abe69101
Juan Posso <juan.posso@epn.edu.ec>
2024-01-19 07:56:42 -0500

    refactor: using functions with typed data and added validation for null and undefined data.

--- scripts/functions.ts
+++ scripts/functions.ts
@@ -1,13 +1,17 @@
 import axios from "axios";
 
-import { IRepository } from "../models/Repository";
+import { Repository as Repository, GitHubResponse } from "../models/Repository";
 
 const API_URL = "https://api.github.com/orgs/stackbuilders/repos";
 
-const getData = async (): Promise<any> => {
+const checkEmptyData = (dataArray: GitHubResponse[]): boolean =>
+  dataArray === undefined || dataArray === null || dataArray.length <= 0;
+
+const getData = async (): Promise<GitHubResponse[] | undefined> => {
   try {
-    const response = await axios.get(API_URL);
-    return await response.data;
+    const response = await axios.get<GitHubResponse[]>(API_URL);
+    if (checkEmptyData(response.data)) return undefined;
+    return response.data;
   } catch (error) {
     console.log(error);
   }
@@ -15,33 +19,33 @@ const getData = async (): Promise<any> => {
 
 // FUCNTIONS TO RETURN THE REPOS WITH MORE THAN FIVE STARS
 
-export const getArrayOfData = async (): Promise<IRepository[]> => {
+export const getArrayOfData = async (): Promise<Repository[]> => {
   const data = await getData();
 
-  return data.map((repo: any) => ({
-    name: repo.name,
-    stargazers_count: repo.stargazers_count,
-    updated_at: repo.updated_at,
-  }));
+  return (
+    data?.map((repo) => ({
+      name: repo.name,
+      stargazers_count: repo.stargazers_count,
+      updated_at: repo.updated_at,
+    })) || []
+  );
 };
 
 export const getReposMoreThanFiveStars = (
-  dataArray: IRepository[]
-): IRepository[] => dataArray.filter((repo) => repo.stargazers_count > 5);
+  dataArray: Repository[]
+): Repository[] => dataArray.filter((repo) => repo.stargazers_count > 5);
 
 // FUCNTIONS TO RETURN THE 5 LAST UPDATED REPOS
 
-export const getLastUpdatedRepos = (
-  dataArray: IRepository[]
-): IRepository[] => {
+export const getLastUpdatedRepos = (dataArray: Repository[]): Repository[] => {
   const sortedData = sortDataByDate(dataArray);
   return getTopFive(sortedData);
 };
 
-const getTopFive = (dataArray: IRepository[]): IRepository[] =>
+export const getTopFive = (dataArray: Repository[]): Repository[] =>
   dataArray.slice(0, 5);
 
-const sortDataByDate = (dataArray: IRepository[]): IRepository[] =>
+export const sortDataByDate = (dataArray: Repository[]): Repository[] =>
   dataArray.sort(
     (a, b) =>
       new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
@@ -49,32 +53,30 @@ const sortDataByDate = (dataArray: IRepository[]): IRepository[] =>
 
 //FUNCTION TO GET THE SUM OF ALL REPOS STARS
 
-export const getSumOfReposStars = (dataArray: IRepository[]): number => {
+export const getSumOfReposStars = (dataArray: Repository[]): number => {
   return dataArray.reduce((acc, repo) => acc + repo.stargazers_count, 0);
 };
 
 //FUNCTIONS TO GET THE TOP 5 REPOS WITH MORE STARS
 
-export const getMostStarredRepos = (
-  dataArray: IRepository[]
-): IRepository[] => {
+export const getMostStarredRepos = (dataArray: Repository[]): Repository[] => {
   const sortedData = sortDataByStarGazers(dataArray);
   return getTopFive(sortedData);
 };
 
-const sortDataByStarGazers = (dataArray: IRepository[]): IRepository[] =>
+export const sortDataByStarGazers = (dataArray: Repository[]): Repository[] =>
   dataArray.sort((a, b) => b.stargazers_count - a.stargazers_count);
 
 //FUNCTIONS TO GET REPOS ALPHABETICALLY AND THAT ITS NAME DOESNT START WITH LETTER "H"
 
 export const getReposAlphabetically = (
-  dataArray: IRepository[]
-): IRepository[] => {
+  dataArray: Repository[]
+): Repository[] => {
   const sortedData = sortDataAlphabetically(dataArray);
   return sortedData.filter((repo) => repo.name[0].toLowerCase() !== "h");
 };
 
-const sortDataAlphabetically = (dataArray: IRepository[]): IRepository[] =>
+export const sortDataAlphabetically = (dataArray: Repository[]): Repository[] =>
   dataArray.sort((a, b) =>
     a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
   );
